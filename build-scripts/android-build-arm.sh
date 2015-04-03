a7f01d742564dde4a710a584f0d443475eb78328
Been Woo <sqbing@gmail.com>
2015-04-03 18:11:47 +0800

    取消对sndfile库的引用

--- build-scripts/android-build-arm.sh
+++ build-scripts/android-build-arm.sh
@@ -1,18 +1,19 @@
 #! /bin/sh
 
-"""
-build the toold chain home with:
-build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=../android-9-toolchain
-"""
+NDK_HOME=~/Library/Android/ndk
+TOOLCHAIN_HOME=~/toolchain
 
-NDK_HOME=~/dev/SDKs/android-9-toolchain-4.9
+# build toolchain first:
+# $(NDK_HOME)/build/tools/make-standalone-toolchain.sh --platform=android-21 --install-dir=$(TOOLCHAIN_HOME) ----toolchain=arm-linux-androideabi-4.6
+
+# build aubio
 set -x
 set -e
 export CC=$NDK_HOME/bin/arm-linux-androideabi-gcc
 export CFLAGS="-I$NDK_HOME/include -mthumb"
-export LDFLAGS="-L$NDK_HOME/lib"
+export LDFLAGS="-L$NDK_HOME/lib "
 export AR=$NDK_HOME/bin/arm-linux-androideabi-ar
 ./waf distclean
-./waf configure --with-target-platform android --disable-jack
+./waf configure --with-target-platform android --disable-jack --disable-avcodec --disable-sndfile
 ./waf build --verbose
 ./waf install --destdir dist-android/arm
